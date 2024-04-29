11f8ef5a267e9c31d1a43e4e2ca5fa45ba102882
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-04-29 13:21:00 +1000

    Added opacity on Anchor Element

--- src/components/UI/Anchor.jsx
+++ src/components/UI/Anchor.jsx
@@ -1,6 +1,6 @@
 function Anchor( {url, img, text}) {
     return (
-        <a className="px-3" href={url} target="_blank"><img src={img} alt={text} /></a>
+        <a className="px-3 opacity-60 hover:opacity-100" href={url} target="_blank"><img src={img} alt={text} /></a>
     );
 }
 
