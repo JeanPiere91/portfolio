951855a265d8a5be3c6630141e350bf3ae9ad524
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-05-04 16:10:52 +1000

    Add border when you hover on the card

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -4,7 +4,7 @@ import github from '../../assets/github.png'
 function Card( {url, path, name, stack}) {
     return (
         <div className="h-64 shadow-2xl relative group">
-            <div className="w-full h-full absolute inset-0 z-10 group-hover:z-30 text-white transition duration-1000">
+            <div className="w-full h-full absolute inset-0 z-10 group-hover:z-30 border-[6px] border-blue-950 text-white transition duration-1000">
                 <AnchorCard url={url} img={github} text="GitHub" name={name} />
                 <h1 className='w-[70%] mx-auto text-lg font-bold tracking-wider font-mono text-blue-950'>{stack}</h1> 
             </div>
